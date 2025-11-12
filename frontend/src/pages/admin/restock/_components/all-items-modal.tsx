7b7a418f7f0c89802ddb8c1a056a6df08712f9cb
Lolillya <someonesusernam@gmail.com>
2025-11-12 11:19:03 +0800

    added restock view all items modal

---
+++ frontend/src/pages/admin/restock/_components/all-items-modal.tsx
@@ -0,0 +1,9 @@
+export const ShowAllModal = () => {
+  return (
+    <section className="absolute bg-black/40 w-full h-full top-0 left-0 flex justify-center items-center z-50">
+      <div className="w-3/6 h-4/5 bg-white px-20 py-10 rounded-lg border shadow-lg">
+        test
+      </div>
+    </section>
+  );
+};
--- frontend/src/pages/admin/restock/index.tsx
+++ frontend/src/pages/admin/restock/index.tsx
@@ -2,82 +2,86 @@ import { Separator } from "@/components/separator";
 import { useRestockQuery } from "@/features/restock/restock-get-all";
 import { SearchIcon, FilterIcon, PlusIcon, EllipsisIcon } from "@/icons";
 import { useNavigate } from "react-router-dom";
+import { ShowAllModal } from "./_components/all-items-modal";
 
 const RestockPage = () => {
   const navigate = useNavigate();
   const { data: restockItems } = useRestockQuery();
   console.log(restockItems);
   return (
-    <section>
-      <div className="w-full mb-8">
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center gap-3 max-w-lg w-full shrink-0">
-            <div className="relative w-full">
-              <input placeholder="Search..." className="input-style-2" />
-              <i className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-                <SearchIcon />
-              </i>
+    <>
+      <ShowAllModal />
+      <section>
+        <div className="w-full mb-8">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-3 max-w-lg w-full shrink-0">
+              <div className="relative w-full">
+                <input placeholder="Search..." className="input-style-2" />
+                <i className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+                  <SearchIcon />
+                </i>
+              </div>
+
+              <div className="p-3 bg-custom-gray rounded-lg">
+                <FilterIcon />
+              </div>
             </div>
 
-            <div className="p-3 bg-custom-gray rounded-lg">
-              <FilterIcon />
+            <div className="flex w/full justify-end gap-2">
+              <button
+                className="flex items-center justify-center gap-2"
+                onClick={() => navigate("/admin/restock/new")}
+              >
+                <PlusIcon />
+                new restock
+              </button>
             </div>
-          </div>
-
-          <div className="flex w/full justify-end gap-2">
-            <button
-              className="flex items-center justify-center gap-2"
-              onClick={() => navigate("/admin/restock/new")}
-            >
-              <PlusIcon />
-              new restock
-            </button>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-5 overflow-y-scroll pb-5 pr-2">
-        {restockItems?.map((r, i) => (
-          <div
-            key={i}
-            className="flex flex-col justify-between gap-5 border shadow-lg rounded-lg p-5"
-          >
-            <div className="flex flex-1 p-3">
-              <div className="flex flex-col gap-3 w-full">
-                <div className="flex gap-3">
-                  <span>#{r.restock_Id}</span>
-                  <span>-</span>
-                  <span>ADD DATE TO RESTOCK MODEL</span>
+        <div className="flex flex-col gap-5 overflow-y-scroll pb-5 pr-2">
+          {restockItems?.map((r, i) => (
+            <div
+              key={i}
+              className="flex flex-col justify-between gap-5 border shadow-lg rounded-lg p-5"
+            >
+              <div className="flex flex-1 p-3">
+                <div className="flex flex-col gap-3 w-full">
+                  <div className="flex gap-3">
+                    <span>#{r.restock_Id}</span>
+                    <span>-</span>
+                    <span>ADD DATE TO RESTOCK MODEL</span>
+                  </div>
+
+                  <div className="flex gap-3">
+                    <span>{r.supplier.company_Name}</span>
+                  </div>
                 </div>
 
-                <div className="flex gap-3">
-                  <span>{r.supplier.company_Name}</span>
+                <Separator orientation="vertical" />
+
+                <div className="flex flex-col gap-3 w-full">
+                  <div className="flex gap-3">
+                    <span>grand total</span>
+                  </div>
+
+                  <div className="flex gap-3">
+                    <span>P {r.grand_total}</span>
+                  </div>
+                </div>
+
+                <div className="bg-gray-bg border flex items-center justify-center rounded-lg p-2 h-12 w-12 my-auto">
+                  <EllipsisIcon />
                 </div>
               </div>
 
-              <Separator orientation="vertical" />
-
-              <div className="flex flex-col gap-3 w-full">
-                <div className="flex gap-3">
-                  <span>grand total</span>
-                </div>
-
-                <div className="flex gap-3">
-                  <span>P {r.grand_total}</span>
-                </div>
-              </div>
-
-              <div className="bg-gray-bg border flex items-center justify-center rounded-lg p-2 h-12 w-12 my-auto">
-                <EllipsisIcon />
+              <div className="flex justify-center">
+                <button>view all items</button>
               </div>
             </div>
-
-            <div className="flex justify-center">
-              <span>view all</span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
