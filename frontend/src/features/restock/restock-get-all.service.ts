ff6b6bd563dd8f4b519d5cd3df1e511c6ed1d0d5
Lolillya <someonesusernam@gmail.com>
2025-11-19 13:06:01 +0800

    fixed restock RestockGetAll exports not found

--- frontend/src/features/restock/restock-get-all.service.ts
+++ frontend/src/features/restock/restock-get-all.service.ts
@@ -1,13 +1,13 @@
-// import { handleError } from "@/helpers/error-handler.helper";
-// import axios from "axios";
-// import { api } from "../api/API.service";
-// import { RestockItemsModel } from "@/models/restock-items.model";
+import { handleError } from "@/helpers/error-handler.helper";
+import axios from "axios";
+import { api } from "../api/API.service";
+import { RestockAllModel } from "./models/restock-all.model";
 
-// export const GetAllRestocks = async () => {
-//   try {
-//     const data = await axios.get<RestockItemsModel[]>(api + "restock/get-all");
-//     return data;
-//   } catch (e) {
-//     handleError(e);
-//   }
-// };
+export const GetAllRestocks = async () => {
+  try {
+    const data = await axios.get<RestockAllModel[]>(api + "restock/get-all");
+    return data;
+  } catch (e) {
+    handleError(e);
+  }
+};
