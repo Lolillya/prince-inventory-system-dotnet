13a5b7d1034c8dfe1689bde0585fd9088b71fad6
Lolillya <someonesusernam@gmail.com>
2025-11-27 18:09:48 +0800

    fixed add product modal flexbox

--- frontend/src/pages/admin/inventory/add-product/_components/AddProductModal.tsx
+++ frontend/src/pages/admin/inventory/add-product/_components/AddProductModal.tsx
@@ -1,19 +1,24 @@
 import { XIcon } from "@/icons";
 import AddProductForm from "./forms/AddProductForm";
+import { Separator } from "@/components/separator";
 
 export const AddProductModal = () => {
   return (
     <div className="absolute bg-black/40 w-full h-full top-0 left-0 flex justify-center items-center z-50">
-      <div className="w-3/6 h-4/5 bg-white px-20 py-10 rounded-lg border shadow-lg relative">
-        <div className="absolute top-4 right-4">
-          <XIcon />
+      <div className="w-3/6 h-4/5 bg-white px-20 py-10 rounded-lg border shadow-lg relative flex flex-col gap-4">
+        <div>
+          <div className="absolute top-4 right-4">
+            <XIcon />
+          </div>
+          <div className="w-full">
+            <h1 className="text-2xl font-bold">Add New Product</h1>
+            <p className="text-gray-500">
+              Fill in the details to add a new product to the inventory.
+            </p>
+          </div>
         </div>
-        <div className="w-full">
-          <h1 className="text-2xl font-bold">Add New Product</h1>
-          <p className="text-gray-500">
-            Fill in the details to add a new product to the inventory.
-          </p>
-        </div>
+
+        <Separator />
 
         <AddProductForm />
       </div>
--- frontend/src/pages/admin/inventory/add-product/_components/forms/AddProductForm.tsx
+++ frontend/src/pages/admin/inventory/add-product/_components/forms/AddProductForm.tsx
@@ -65,138 +65,146 @@ const AddProductForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <div>
-        <label htmlFor="productName" className="block text-sm font-medium">
-          Product Name
-        </label>
-        <input
-          id="productName"
-          type="text"
-          {...register("productName")}
-          className="w-full drop-shadow-none bg-custom-gray p-2"
-        />
-        {errors.productName && (
-          <p className="text-red-500 text-xs mt-1">
-            {errors.productName.message}
-          </p>
-        )}
-      </div>
-      <div>
-        <label htmlFor="description" className="block text-sm font-medium">
-          Description
-        </label>
-        <textarea
-          id="description"
-          {...register("description")}
-          className="w-full p-2 rounded-lg "
-        />
-        {errors.description && (
-          <p className="text-red-500 text-xs mt-1">
-            {errors.description.message}
-          </p>
-        )}
-      </div>
-      <div>
-        <label htmlFor="productCode" className="block text-sm font-medium">
-          Product Code
-        </label>
-        <input
-          id="productCode"
-          type="text"
-          {...register("productCode")}
-          className="w-full drop-shadow-none bg-custom-gray p-2"
-        />
-        {errors.productCode && (
-          <p className="text-red-500 text-xs mt-1">
-            {errors.productCode.message}
-          </p>
-        )}
-      </div>
-      {/* BRANDS */}
-      <div>
-        <label htmlFor="brandId" className="block text-sm font-medium">
-          Brand
-        </label>
-        <select
-          id="brandId"
-          {...register("brandId")}
-          disabled={productFieldsLoading}
-          className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
-        >
-          <option value="">
-            {productFieldsLoading ? "Loading..." : "Select Brand"}
-          </option>
-          {productFields?.map((brand, i) => (
-            <option key={i} value={brand.brandName}>
-              {brand.brandName}
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className=" flex flex-col gap-5 overflow-y-scroll flex-1"
+    >
+      <div className="flex flex-col space-y-4 mb-auto">
+        {/* PRODUCT NAME */}
+        <div>
+          <label htmlFor="productName" className="block text-sm font-medium">
+            Product Name
+          </label>
+          <input
+            id="productName"
+            type="text"
+            {...register("productName")}
+            className="w-full drop-shadow-none bg-custom-gray p-2"
+          />
+          {errors.productName && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.productName.message}
+            </p>
+          )}
+        </div>
+
+        {/* DESCRIPTION */}
+        <div>
+          <label htmlFor="description" className="block text-sm font-medium">
+            Description
+          </label>
+          <textarea
+            id="description"
+            {...register("description")}
+            className="w-full p-2 rounded-lg "
+          />
+          {errors.description && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.description.message}
+            </p>
+          )}
+        </div>
+
+        {/* PRODUCT CODE */}
+        <div>
+          <label htmlFor="productCode" className="block text-sm font-medium">
+            Product Code
+          </label>
+          <input
+            id="productCode"
+            type="text"
+            {...register("productCode")}
+            className="w-full drop-shadow-none bg-custom-gray p-2"
+          />
+          {errors.productCode && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.productCode.message}
+            </p>
+          )}
+        </div>
+        {/* BRANDS */}
+        <div>
+          <label htmlFor="brandId" className="block text-sm font-medium">
+            Brand
+          </label>
+          <select
+            id="brandId"
+            {...register("brandId")}
+            disabled={productFieldsLoading}
+            className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
+          >
+            <option value="">
+              {productFieldsLoading ? "Loading..." : "Select Brand"}
             </option>
-          ))}
-        </select>
-        {errors.brandId && (
-          <p className="text-red-500 text-xs mt-1">{errors.brandId.message}</p>
-        )}
+            {productFields?.map((brand, i) => (
+              <option key={i} value={brand.brandName}>
+                {brand.brandName}
+              </option>
+            ))}
+          </select>
+          {errors.brandId && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.brandId.message}
+            </p>
+          )}
+        </div>
+
+        {/* CATEGORY */}
+        <div>
+          <label htmlFor="categoryId" className="block text-sm font-medium">
+            Category
+          </label>
+          <select
+            id="categoryId"
+            {...register("categoryId")}
+            className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
+            disabled={productFieldsLoading}
+          >
+            <option value="">
+              {productFieldsLoading ? "Loading..." : "Select Category"}
+            </option>
+            {productFields?.map((category, i) => (
+              <option key={i} value={category.category_Name}>
+                {category.category_Name}
+              </option>
+            ))}
+          </select>
+          {errors.categoryId && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.categoryId.message}
+            </p>
+          )}
+        </div>
+
+        {/* VARIANT */}
+        <div>
+          <label htmlFor="variantId" className="block text-sm font-medium">
+            Variant
+          </label>
+          <select
+            id="variantId"
+            {...register("variantId")}
+            className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
+            disabled={productFieldsLoading}
+          >
+            <option value="">
+              {productFieldsLoading ? "Loading..." : "Select Variant"}
+            </option>
+            {productFields?.map((variant, i) => (
+              <option key={i} value={variant.variant_Name}>
+                {variant.variant_Name}
+              </option>
+            ))}
+          </select>
+          {errors.variantId && (
+            <p className="text-red-500 text-xs mt-1">
+              {errors.variantId.message}
+            </p>
+          )}
+        </div>
       </div>
 
-      {/* CATEGORY */}
-      <div>
-        <label htmlFor="categoryId" className="block text-sm font-medium">
-          Category
-        </label>
-        <select
-          id="categoryId"
-          {...register("categoryId")}
-          className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
-          disabled={productFieldsLoading}
-        >
-          <option value="">
-            {productFieldsLoading ? "Loading..." : "Select Category"}
-          </option>
-          {productFields?.map((category, i) => (
-            <option key={i} value={category.category_Name}>
-              {category.category_Name}
-            </option>
-          ))}
-        </select>
-        {errors.categoryId && (
-          <p className="text-red-500 text-xs mt-1">
-            {errors.categoryId.message}
-          </p>
-        )}
-      </div>
-
-      {/* VARIANT */}
-      <div>
-        <label htmlFor="variantId" className="block text-sm font-medium">
-          Variant
-        </label>
-        <select
-          id="variantId"
-          {...register("variantId")}
-          className="rounded-lg w-full p-2 text-sm drop-shadow-none bg-custom-bg-white"
-          disabled={productFieldsLoading}
-        >
-          <option value="">
-            {productFieldsLoading ? "Loading..." : "Select Variant"}
-          </option>
-          {productFields?.map((variant, i) => (
-            <option key={i} value={variant.variant_Name}>
-              {variant.variant_Name}
-            </option>
-          ))}
-        </select>
-        {errors.variantId && (
-          <p className="text-red-500 text-xs mt-1">
-            {errors.variantId.message}
-          </p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-        disabled={mutation.isPending}
-      >
+      <button type="submit" disabled={mutation.isPending}>
         {mutation.isPending ? "Adding Product..." : "Add Product"}
       </button>
     </form>
