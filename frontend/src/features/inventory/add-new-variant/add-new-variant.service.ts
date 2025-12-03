53a58f096bdb99085ff167e3e0cad9903f9dd2c5
Lolillya <someonesusernam@gmail.com>
2025-12-03 19:13:23 +0800

    added service for POST request to new-variant endpoint

--- frontend/src/features/inventory/add-new-variant/add-new-variant.service.ts
+++ frontend/src/features/inventory/add-new-variant/add-new-variant.service.ts
@@ -2,9 +2,9 @@ import { api } from "@/features/api/API.service";
 import { handleError } from "@/helpers/error-handler.helper";
 import axios from "axios";
 
-export const addNewVariantService = async (payload: { name: string }) => {
+export const addNewVariantService = async (variantName: string) => {
   try {
-    const res = await axios.post(api + "add-variant", payload);
+    const res = await axios.post(api + "add-variant", { variantName });
     return res.data;
   } catch (err) {
     handleError(err);
