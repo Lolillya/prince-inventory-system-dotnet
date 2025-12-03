993efcc47ae61d0a568831529068cfb9e63ac474
Lolillya <someonesusernam@gmail.com>
2025-12-03 19:14:08 +0800

    updated new-variant form; fixed unsupported media type error

--- frontend/src/pages/admin/inventory/add-product/_components/forms/AddVariantForm.tsx
+++ frontend/src/pages/admin/inventory/add-product/_components/forms/AddVariantForm.tsx
@@ -1,3 +1,7 @@
+import { addNewVariantService } from "@/features/inventory/add-new-variant/add-new-variant.service";
+import { useState } from "react";
+import { set } from "react-hook-form";
+
 interface AddVariantFormProps {
   setIsVariantModalOpen: (isOpen: boolean) => void;
   setIsAddProductModalOpen: (isOpen: boolean) => void;
@@ -12,6 +16,15 @@ export const AddVariantForm = ({
     setIsAddProductModalOpen(true);
   };
 
+  const [variantName, setVariantName] = useState<string>("");
+
+  const handleAddVariant = async (event: React.FormEvent) => {
+    addNewVariantService(variantName);
+    setIsVariantModalOpen(false);
+    setIsAddProductModalOpen(true);
+    event.preventDefault();
+  };
+
   return (
     <form className=" flex flex-col gap-5 overflow-y-scroll flex-1 justify-between">
       <div>
@@ -22,6 +35,7 @@ export const AddVariantForm = ({
           id="variant_Name"
           type="text"
           className="w-full drop-shadow-none bg-custom-gray p-2"
+          onChange={(e) => setVariantName(e.target.value)}
           // {...register("brand_Name")}
         />
         <span className="text-red-500 text-xs normal-case">
@@ -29,7 +43,9 @@ export const AddVariantForm = ({
         </span>
       </div>
       <div className="flex gap-2">
-        <button type="submit">Add Variant</button>
+        <button type="submit" onClick={handleAddVariant}>
+          Add Variant
+        </button>
         <button type="button" className="input-style-3" onClick={handleCancel}>
           Cancel
         </button>
