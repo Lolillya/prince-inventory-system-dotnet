a11f8e9378078e33987ed6edd7e945e67329696c
Lolillya <someonesusernam@gmail.com>
2025-12-03 19:12:21 +0800

    added DTO for add-variant endpoint payload

---
+++ backend/Dtos/Inventory/VariantDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Dtos.Inventory
+{
+    public class VariantDto
+    {
+        public string VariantName { get; set; } = "";
+    }
+}
