cd46fcb99c2512d863f2802ec25af53ca299b43c
Lolillya <someonesusernam@gmail.com>
2025-12-03 19:13:04 +0800

    added service for POST request to new-category endpoint

--- frontend/src/features/inventory/add-new-category/add-new-category.service.ts
+++ frontend/src/features/inventory/add-new-category/add-new-category.service.ts
@@ -2,9 +2,9 @@ import { api } from "@/features/api/API.service";
 import { handleError } from "@/helpers/error-handler.helper";
 import axios from "axios";
 
-export const addNewCategoryService = async (payload: { name: string }) => {
+export const addNewCategoryService = async (categoryName: string) => {
   try {
-    const res = await axios.post(api + "add-category", payload);
+    const res = await axios.post(api + "add-category", { categoryName });
     return res.data;
   } catch (err) {
     handleError(err);
