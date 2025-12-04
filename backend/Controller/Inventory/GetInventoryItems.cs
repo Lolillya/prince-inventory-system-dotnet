f873c5092e2077b2552008a28a0410da4ff5cda6
Lolillya <someonesusernam@gmail.com>
2025-12-04 20:28:56 +0800

    modified return values excluding brand, variant, and category in product object

--- backend/Controller/Inventory/GetInventoryItems.cs
+++ backend/Controller/Inventory/GetInventoryItems.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
-using System.Numerics;
-using DtoBrand = backend.Dtos.Inventory.Brand;
-using DtoCategory = backend.Dtos.Inventory.Category;
-using DtoProduct = backend.Dtos.Inventory.Product;
-using DtoVariant = backend.Dtos.Inventory.Variant;
-using DtoInventory = backend.Dtos.Inventory.Inventory;
+using backend.Models.Inventory;
 
 namespace backend.Controller.Inventory
 {
@@ -32,36 +24,38 @@ namespace backend.Controller.Inventory
             {
                 var results = await _db.Inventory
                     .Include(i => i.Product)
-                        .ThenInclude(i => i.Brand)
-                    .Include(i => i.Product)
-                        .ThenInclude(i => i.Variant)
-                    .Include(i => i.Product)
-                        .ThenInclude(i => i.Category)
                     .Select(i => new
                     {
-                        Product = new DtoProduct
+                        Product = new
                         {
-                            Product_ID = i.Product_ID,
-                            ProductCode = i.Product.Product_Code,
-                            ProductName = i.Product.Product_Name,
-                            Description = i.Product.Description,
-                            Brand_Id = i.Product.Brand.Brand_ID,
-                            Category_Id = i.Product.Category.Category_ID,
+                            i.Product_ID,
+                            i.Product.Product_Code,
+                            i.Product.Product_Name,
+                            i.Product.Description,
                             CreatedAt = i.Created_At,
                             UpdatedAt = i.Updated_At
                         },
-                        Variant = new DtoVariant
+                        Variant = new
                         {
-                            VariantName = i.Product.Variant.Variant_Name,
-                            CreatedAt = i.Product.Variant.CreatedAt,
-                            UpdatedAt = i.Product.Variant.UpdatedAt
+                            i.Product.Variant.Variant_ID,
+                            i.Product.Variant.Variant_Name,
+                            i.Product.Variant.CreatedAt,
+                            i.Product.Variant.UpdatedAt
                         },
-                        Brand = new DtoBrand
+                        Brand = new
                         {
-                            BrandName = i.Product.Brand.BrandName,
-                            CreatedAt = i.Product.Brand.CreatedAt,
-                            UpdatedAt = i.Product.Brand.UpdatedAt
+                            i.Product.Brand.Brand_ID,
+                            i.Product.Brand.BrandName,
+                            i.Product.Brand.CreatedAt,
+                            i.Product.Brand.UpdatedAt
                         },
+                        Category = new
+                        {
+                            i.Product.Category.Category_ID,
+                            i.Product.Category.Category_Name,
+                            i.Product.Category.CreatedAt,
+                            i.Product.Category.UpdatedAt
+                        }
                     }).ToListAsync();
 
                 if (results == null || !results.Any())
