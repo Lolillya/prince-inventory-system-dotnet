7abfdd1530ab66b4b567c590d7f91d4e4cd3bdf3
Lolillya <someonesusernam@gmail.com>
2025-12-16 15:11:30 +0800

    new feature: add new employee

--- frontend/src/pages/admin/employees/index.tsx
+++ frontend/src/pages/admin/employees/index.tsx
@@ -1,27 +1,57 @@
-import React from "react";
-import { InfoCard } from "../../../components/info-card";
-import { NoSelectedState } from "../../../components/no-selected-state";
-import { SelectedUser } from "../../../components/selected-user";
-import { Separator } from "../../../components/separator";
-import { FilterIcon, PlusIcon, SearchIcon } from "../../../icons";
-import { userEmployeesQuery } from "../../../features/employees/employee-get-all.query";
-import { useSelectedEmployeeQuery } from "../../../features/employees/empployee-selected.query";
+import { Fragment, useState } from "react";
+import { InfoCard } from "@/components/info-card";
+import { NoSelectedState } from "@/components/no-selected-state";
+import { SelectedUser } from "@/components/selected-user";
+import { Separator } from "@/components/separator";
+import { FilterIcon, PlusIcon, SearchIcon } from "@/icons";
+import { userEmployeesQuery } from "@/features/employees/employee-get-all.query";
+import { useSelectedEmployeeQuery } from "@/features/employees/empployee-selected.query";
+import { AddEmployeeModal } from "./_components/add-employee.modal";
 
 const EmployeesPage = () => {
   const { data: employees, isLoading, error } = userEmployeesQuery();
   const { data: selectedEmployee } = useSelectedEmployeeQuery();
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isAddEmployeeModalOpen, setIsAddEmployeeModalOpen] = useState(false);
+
   // FETCH DATA LOADING STATE
   if (isLoading) return <div>Loading...</div>;
   // FETCHING DATA ERROR STATE
   if (error) return <div>Error...</div>;
+
+  const filteredEmployees = employees?.filter((employee) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      employee.firstName.toLowerCase().includes(query) ||
+      employee.lastName.toLowerCase().includes(query) ||
+      employee.email.toLowerCase().includes(query) ||
+      employee.companyName.toLowerCase().includes(query) ||
+      employee.phoneNumber.toLowerCase().includes(query)
+    );
+  });
+
+  const handleAddEmployee = () => {
+    setIsAddEmployeeModalOpen(!isAddEmployeeModalOpen);
+  };
+
   return (
     <section>
+      {/* ADD EMPLOYEE MODAL */}
+      {isAddEmployeeModalOpen && (
+        <AddEmployeeModal
+          setIsAddEmployeeModalOpen={setIsAddEmployeeModalOpen}
+        />
+      )}
       <div className="w-full mb-8">
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-3 max-w-lg w-full shrink-0">
             <div className="relative w-full">
-              <input placeholder="Search..." className="input-style-2" />
+              <input
+                placeholder="Search..."
+                className="input-style-2"
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
               <i className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
                 <SearchIcon />
               </i>
@@ -31,7 +61,10 @@ const EmployeesPage = () => {
               <FilterIcon />
             </div>
           </div>
-          <button className="flex items-center justify-center gap-2">
+          <button
+            className="flex items-center justify-center gap-2"
+            onClick={handleAddEmployee}
+          >
             <PlusIcon />
             new supplier
           </button>
@@ -45,15 +78,17 @@ const EmployeesPage = () => {
             <label className="capitalize text-saltbox-gray font-normal text-lg">
               suppiers
             </label>
-            <span className="capitalize text-vesper-gray"># records</span>
+            <span className="capitalize text-vesper-gray">
+              {filteredEmployees?.length} records
+            </span>
           </div>
 
           <div className="w-full overflow-y-scroll">
-            {employees?.map((data, index) => (
-              <React.Fragment key={data.id}>
+            {filteredEmployees?.map((data, index) => (
+              <Fragment key={data.id}>
                 <InfoCard type="employee" key={index} {...data} />
                 <Separator />
-              </React.Fragment>
+              </Fragment>
             ))}
           </div>
         </div>
