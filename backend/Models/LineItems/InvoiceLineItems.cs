4265f4900e4541f8403e47c91dbe2f31fa5a72e6
Lolillya <someonesusernam@gmail.com>
2025-12-21 21:42:20 +0800

    modified: backend invoice line items

--- backend/Models/LineItems/InvoiceLineItems.cs
+++ backend/Models/LineItems/InvoiceLineItems.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using backend.Models.Inventory;
 using backend.Models.InvoiceModel;
+using backend.Models.Unit;
 
 namespace backend.Models.LineItems
 {
@@ -17,6 +18,9 @@ namespace backend.Models.LineItems
         public Product Product { get; set; } = null!;
         [ForeignKey(nameof(Invoice_ID))]
         public Invoice Invoices { get; set; } = null!;
+        public int UOM_ID { get; set; }
+        [ForeignKey(nameof(UOM_ID))]
+        public UnitOfMeasure UnitOfMeasure { get; set; } = null!;
 
         public string Unit { get; set; } = null!;
         [Column(TypeName = "decimal(18,2)")]
@@ -24,5 +28,7 @@ namespace backend.Models.LineItems
         [Column(TypeName = "decimal(18,2)")]
         public decimal Sub_Total { get; set; }
         public int Unit_Quantity { get; set; }
+        public int Discount { get; set; }
+        public bool isPercentageDiscount { get; set; }
     }
 }
