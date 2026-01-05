e316044790b188998e3f87ea496f5f9fb6d8c76f
Lolillya <someonesusernam@gmail.com>
2026-01-05 18:10:36 +0800

    add: product selector on unit conversion preset selector

--- frontend/src/pages/admin/inventory/_components/preset-editor.modal.tsx
+++ frontend/src/pages/admin/inventory/_components/preset-editor.modal.tsx
@@ -11,6 +11,8 @@ export const ProductUnitPresetModal = ({
   handlePresetEditor,
 }: ProductUnitPresetModalProp) => {
   const [isAddPresetOpen, setIsAddPresetOpen] = useState(false);
+  const [isAddProductsToPresetOpen, setIsAddProductsToPresetOpen] =
+    useState(false);
 
   const handleAddPreset = () => {
     setIsAddPresetOpen(!isAddPresetOpen);
@@ -25,8 +27,12 @@ export const ProductUnitPresetModal = ({
     setIsAddPresetOpen(false);
   };
 
+  const handleAddProductsToPreset = () => {
+    setIsAddProductsToPresetOpen(!isAddProductsToPresetOpen);
+  };
+
   return (
-    <div className="absolute bg-black/40 w-full h-full top-0 left-0 flex justify-center items-center z-50">
+    <div className="absolute bg-black/40 w-full h-full top-0 left-0 flex justify-center items-center z-50 gap-3">
       <div
         className={`w-3/6 ${isAddPresetOpen ? "h-4/12" : "h-4/5"} bg-white px-5 py-10 rounded-lg border shadow-lg relative flex flex-col gap-4 transition-all duration-200`}
       >
@@ -35,7 +41,7 @@ export const ProductUnitPresetModal = ({
         </div>
 
         <div className="w-full">
-          <h1 className="">Packaging Presets</h1>
+          <h1>Packaging Presets</h1>
         </div>
 
         <Activity mode={isAddPresetOpen ? "visible" : "hidden"}>
@@ -71,23 +77,12 @@ export const ProductUnitPresetModal = ({
                 <span className="text-sm font-semibold text-saltbox-gray">
                   # Products
                 </span>
-                <span className="text-sm font-semibold hover:underline cursor-pointer">
-                  More
-                </span>
-              </div>
 
-              <div className="flex items-center justify-between p-2 rounded-lg">
-                <span className="text-sm font-semibold text-saltbox-gray">
-                  Box
-                </span>
-                <span className="text-sm font-semibold text-saltbox-gray">
-                  Box &gt; Cases &gt; Pieces
-                </span>
-                <span className="text-sm font-semibold text-saltbox-gray">
-                  # Products
-                </span>
-                <span className="text-sm font-semibold hover:underline cursor-pointer">
-                  Add
+                <span
+                  className="text-sm font-semibold hover:underline cursor-pointer"
+                  onClick={handleAddProductsToPreset}
+                >
+                  {isAddProductsToPresetOpen ? "Close" : "Add"}
                 </span>
               </div>
             </div>
@@ -103,6 +98,40 @@ export const ProductUnitPresetModal = ({
           </div>
         )}
       </div>
+
+      {/*   PRODUCT SELECTOR */}
+      <Activity mode={isAddProductsToPresetOpen ? "visible" : "hidden"}>
+        <div className="bg-white h-4/5 w-3/12 rounded-lg border shadow-lg py-10 px-5 flex flex-col justify-between">
+          <div className="flex flex-col gap-5">
+            <div className="flex items-center justify-between">
+              <h3 className="font-bold">Select Product/s</h3>
+
+              <label className="text-saltbox-gray text-sm">
+                # products selected
+              </label>
+            </div>
+
+            <div className="flex flex-col overflow-y-scroll gap-3 border p-3 rounded-lg inset-shadow-sm">
+              {/* PRODUCT SELECTOR FORM */}
+              <div className="flex items-center gap-3">
+                <input type="checkbox" />
+                <div className="flex items-center gap-3">
+                  <span>Product Name</span>
+                  <span>-</span>
+                  <span>Brand</span>
+                  <span>-</span>
+                  <span>Variant</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex w-full gap-3">
+            <button className="w-full max-w-full">Add Product</button>
+            <button className="w-full max-w-full">Cancel</button>
+          </div>
+        </div>
+      </Activity>
     </div>
   );
 };
