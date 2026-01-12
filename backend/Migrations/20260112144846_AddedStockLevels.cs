96fcbef9abb78183a3351447944589dc0c6f0813
Lolillya <someonesusernam@gmail.com>
2026-01-12 22:51:26 +0800

    add: db migrations for the updated product unit preset db table

---
+++ backend/Migrations/20260112144846_AddedStockLevels.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStockLevels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Low_Stock_Level",
+                table: "Product_Unit_Presets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Very_Low_Stock_Level",
+                table: "Product_Unit_Presets",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Low_Stock_Level",
+                table: "Product_Unit_Presets");
+
+            migrationBuilder.DropColumn(
+                name: "Very_Low_Stock_Level",
+                table: "Product_Unit_Presets");
+        }
+    }
+}
