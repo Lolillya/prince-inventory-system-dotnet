f2a8980089701d41490ea14693616bba2d4318c7
Lolillya <someonesusernam@gmail.com>
2026-01-14 01:22:13 +0800

    add: hover card on new restock product card showing unit preset on hover

--- frontend/src/pages/admin/restock/_components/product-card.tsx
+++ frontend/src/pages/admin/restock/_components/product-card.tsx
@@ -1,3 +1,8 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { InventoryBatchesModel } from "@/features/restock/models/inventory-batches.model";
 
 interface ProductCardProps {
@@ -17,7 +22,25 @@ export const ProductCard = ({ product, onClick }: ProductCardProps) => {
           <span>{product.brand.brandName}</span>
           <span>{product.variant.variant_Name}</span>
         </div>
-        <span>{product.unitPresets.length} preset(s)</span>
+        <HoverCard>
+          <HoverCardTrigger>
+            {product.unitPresets.length} Preset(s)
+          </HoverCardTrigger>
+          <HoverCardContent className="flex items-center gap-1">
+            {product.unitPresets.map((u) =>
+              u.preset.presetLevels.map((l, idx) => (
+                <div key={idx} className="flex gap-1 items-center">
+                  <span className="text-xs font-semibold">
+                    {l.conversion_Factor} {l.unitOfMeasure.uom_Name}
+                  </span>
+                  {idx < u.preset.presetLevels.length - 1 && (
+                    <span className="text-xs font-semibold">&gt;</span>
+                  )}
+                </div>
+              ))
+            )}
+          </HoverCardContent>
+        </HoverCard>
       </div>
     </div>
   );
