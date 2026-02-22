d5334a10680b7f3b6c812917a37198fcd71c228f
Lolillya <someonesusernam@gmail.com>
2026-02-23 00:44:56 +0800

    update: new supplier modal removed username, password, and role. validation requiring company name only

--- frontend/src/pages/admin/suppliers/_components/forms/add-supplier.form.tsx
+++ frontend/src/pages/admin/suppliers/_components/forms/add-supplier.form.tsx
@@ -15,9 +15,9 @@ const schema = yup.object().shape({
     .required("Email address is required")
     .matches(
       /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-      "Invalid email format"
+      "Invalid email format",
     ),
-  phoneNumber: yup.string().required("Contact number is required"),
+  phoneNumber: yup.string().optional(),
   companyName: yup.string().required("Company name is required"),
   address: yup.string().required("Address is required"),
   notes: yup.string().optional(),
@@ -35,133 +35,29 @@ export const AddSupplierForm = () => {
       roleID: 3,
       username: "",
       password: "",
+      phoneNumber: "",
+      companyName: "",
+      address: "",
+      notes: "",
     },
   });
 
-  const onSubmit = (data: UserModel) => {
-    AddNewSupplierService(data);
+  const onSubmit = (data: yup.InferType<typeof schema>) => {
+    AddNewSupplierService(data as UserModel);
   };
 
   return (
     <form
-      className="h-full flex flex-col justify-between"
+      className="h-full flex flex-col justify-between gap-4"
       onSubmit={handleSubmit(onSubmit)}
     >
       <div className="flex flex-col space-y-4 mb-auto">
         {/* LOGIN DETAILS */}
 
-        <div className="flex w-full justify-between gap-4">
-          {/* USERNAME*/}
-          <div className="flex flex-col w-full">
-            <label htmlFor="firstName" className="block text-sm font-medium">
-              Username
-            </label>
-            <input
-              id="firstName"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              placeholder="AUTO GENERATED"
-              disabled
-              {...register("username")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.username?.message}
-            </span>
-          </div>
-
-          {/* PASSWORD */}
-          <div className="flex flex-col w-full">
-            <label htmlFor="password" className="block text-sm font-medium">
-              Password
-            </label>
-            <input
-              id="password"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              placeholder="AUTO GENERATED"
-              disabled
-              {...register("password")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.password?.message}
-            </span>
-          </div>
-        </div>
-
-        {/* SUPPLIER NAME */}
-        <div className="flex w-full justify-between gap-4">
-          {/* FIRST NAME */}
-          <div className="flex flex-col w-full">
-            <label htmlFor="firstName" className="block text-sm font-medium">
-              First Name
-            </label>
-            <input
-              id="firstName"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              {...register("firstName")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.firstName?.message}
-            </span>
-          </div>
-
-          {/* LAST NAME */}
-          <div className="flex flex-col w-full">
-            <label htmlFor="lastName" className="block text-sm font-medium">
-              Last Name
-            </label>
-            <input
-              id="lastName"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              {...register("lastName")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.lastName?.message}
-            </span>
-          </div>
-        </div>
-
-        <div className="flex w-full justify-between gap-4">
-          <div className="flex flex-col w-full">
-            <label htmlFor="emailAddress" className="block text-sm font-medium">
-              Email Address
-            </label>
-            <input
-              id="emailAddress"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              {...register("email")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.email?.message}
-            </span>
-          </div>
-
-          <div className="flex flex-col w-full">
-            <label
-              htmlFor="contactNumber"
-              className="block text-sm font-medium"
-            >
-              Contact Number
-            </label>
-            <input
-              id="contactNumber"
-              type="text"
-              className="w-full drop-shadow-none bg-custom-gray p-2"
-              {...register("phoneNumber")}
-            />
-            <span className="text-red-500 text-xs normal-case">
-              {errors.phoneNumber?.message}
-            </span>
-          </div>
-        </div>
-
         {/* COMPANY NAME */}
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full gap-2">
           <label htmlFor="companyName" className="block text-sm font-medium">
-            Company Name
+            Supplier Company Name
           </label>
           <input
             id="companyName"
@@ -174,8 +70,85 @@ export const AddSupplierForm = () => {
           </span>
         </div>
 
+        {/* SUPPLIER NAME */}
+        <div className="flex flex-col w-full justify-between gap-2">
+          {/* FIRST NAME */}
+          <label htmlFor="firstName" className="block text-sm font-medium">
+            Representative
+          </label>
+
+          <div className="flex gap-4">
+            <div className="flex flex-col w-full">
+              <input
+                id="firstName"
+                type="text"
+                className="w-full drop-shadow-none bg-custom-gray p-2"
+                placeholder="John"
+                {...register("firstName")}
+              />
+              {/* <span className="text-red-500 text-xs normal-case">
+                {errors.firstName?.message}
+              </span> */}
+            </div>
+
+            {/* LAST NAME */}
+            <div className="flex flex-col w-full">
+              {/* <label htmlFor="lastName" className="block text-sm font-medium">
+              Last Name
+            </label> */}
+              <input
+                id="lastName"
+                type="text"
+                className="w-full drop-shadow-none bg-custom-gray p-2"
+                placeholder="Doe"
+                {...register("lastName")}
+              />
+              {/* <span className="text-red-500 text-xs normal-case">
+                {errors.lastName?.message}
+              </span> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-between gap-4">
+          <div className="flex flex-col w-full gap-2">
+            <label
+              htmlFor="contactNumber"
+              className="block text-sm font-medium"
+            >
+              Contact Number
+            </label>
+            <input
+              id="contactNumber"
+              type="text"
+              className="w-full drop-shadow-none bg-custom-gray p-2"
+              placeholder="09xxxxxxxxx"
+              {...register("phoneNumber")}
+            />
+            {/* <span className="text-red-500 text-xs normal-case">
+              {errors.phoneNumber?.message}
+            </span> */}
+          </div>
+
+          <div className="flex flex-col w-full gap-2">
+            <label htmlFor="emailAddress" className="block text-sm font-medium">
+              Email Address
+            </label>
+            <input
+              id="emailAddress"
+              type="text"
+              className="w-full drop-shadow-none bg-custom-gray p-2"
+              placeholder="example.email.com"
+              {...register("email")}
+            />
+            {/* <span className="text-red-500 text-xs normal-case">
+              {errors.email?.message}
+            </span> */}
+          </div>
+        </div>
+
         {/* ADDRESS */}
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full gap-2">
           <label htmlFor="address" className="block text-sm font-medium">
             Address
           </label>
@@ -185,13 +158,13 @@ export const AddSupplierForm = () => {
             className="w-full drop-shadow-none bg-custom-gray p-2"
             {...register("address")}
           />
-          <span className="text-red-500 text-xs normal-case">
+          {/* <span className="text-red-500 text-xs normal-case">
             {errors.address?.message}
-          </span>
+          </span> */}
         </div>
 
         {/* ROLE */}
-        <div className="flex flex-col w-full">
+        {/* <div className="flex flex-col w-full">
           <label htmlFor="role" className="block text-sm font-medium">
             Role
           </label>
@@ -201,22 +174,24 @@ export const AddSupplierForm = () => {
             className="w-full drop-shadow-none bg-custom-gray p-2"
             disabled
             placeholder="SUPPLIER"
-            // {...register("role")}
+            
           />
-        </div>
+        </div> */}
         {/* DESCRIPTION */}
-        <div>
+        <div className="flex flex-col gap-2">
           <label htmlFor="supplierNotes" className="block text-sm font-medium">
-            Supplier Notes
+            Supplier Note
           </label>
           <textarea
             id="supplierNotes"
             className="w-full p-2 rounded-lg "
+            placeholder="About this supplier..."
             {...register("notes")}
+            rows={4}
           />
-          <span className="text-red-500 text-xs normal-case">
+          {/* <span className="text-red-500 text-xs normal-case">
             {errors.notes?.message}
-          </span>
+          </span> */}
         </div>
       </div>
 
