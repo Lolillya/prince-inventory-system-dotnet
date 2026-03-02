7d181c34573454b9ed8152beefca8b13fd8e5578
Lolillya <someonesusernam@gmail.com>
2026-03-02 21:00:36 +0800

    fix: revert commit; fixed imports

--- frontend/src/pages/admin/inventory/_components/forms/edit-product.form.tsx
+++ frontend/src/pages/admin/inventory/_components/forms/edit-product.form.tsx
@@ -14,19 +14,19 @@ const schema = yup.object().shape({
   brand_ID: yup
     .number()
     .transform((value, originalValue) =>
-      String(originalValue).trim() === "" ? undefined : value
+      String(originalValue).trim() === "" ? undefined : value,
     )
     .required("Brand is required"),
   category_Id: yup
     .number()
     .transform((value, originalValue) =>
-      String(originalValue).trim() === "" ? undefined : value
+      String(originalValue).trim() === "" ? undefined : value,
     )
     .required("Category is required"),
   variant_Id: yup
     .number()
     .transform((value, originalValue) =>
-      String(originalValue).trim() === "" ? undefined : value
+      String(originalValue).trim() === "" ? undefined : value,
     )
     .required("Variant is required"),
   unitPresets: yup.array().of(
@@ -34,7 +34,7 @@ const schema = yup.object().shape({
       low_Stock_Level: yup
         .number()
         .transform((value, originalValue) =>
-          String(originalValue).trim() === "" ? undefined : value
+          String(originalValue).trim() === "" ? undefined : value,
         )
         .typeError("Must be a number")
         .positive("Must be a positive number")
@@ -43,13 +43,13 @@ const schema = yup.object().shape({
       very_Low_Stock_Level: yup
         .number()
         .transform((value, originalValue) =>
-          String(originalValue).trim() === "" ? undefined : value
+          String(originalValue).trim() === "" ? undefined : value,
         )
         .typeError("Must be a number")
         .positive("Must be a positive number")
         .integer("Must be a whole number")
         .required("Very low stock level is required"),
-    })
+    }),
   ),
 });
 
@@ -58,7 +58,10 @@ interface EditProductFormProps {
   setIsEditProductModalOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const EditProductForm = ({ selectedProduct, setIsEditProductModalOpen }: EditProductFormProps }: EditProductFormProps) => {
+export const EditProductForm = ({
+  selectedProduct,
+  setIsEditProductModalOpen,
+}: EditProductFormProps) => {
   const {
     register,
     handleSubmit,
