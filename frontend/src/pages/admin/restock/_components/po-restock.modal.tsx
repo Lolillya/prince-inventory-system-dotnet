a2d2e905acaa65ff38096cdd0cd103ef4429c39f
Lolillya <someonesusernam@gmail.com>
2026-03-20 02:44:03 +0800

    added PO restock modal component

---
+++ frontend/src/pages/admin/restock/_components/po-restock.modal.tsx
@@ -0,0 +1,9 @@
+export const PO_RestockModal = () => {
+  return (
+    <section className="absolute bg-black/40 w-full h-full top-0 left-0 flex justify-center items-center z-50">
+      <div className="w-3/6 h-4/5 bg-white p-10 rounded-lg border shadow-lg">
+        <label className="text-xl"></label>
+      </div>
+    </section>
+  );
+};
