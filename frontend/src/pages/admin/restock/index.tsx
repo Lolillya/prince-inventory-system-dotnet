55db09d8485da7d79c01d012ddb0431f54afb537
Lolillya <someonesusernam@gmail.com>
2026-05-27 13:50:26 +0800

    updated restock card layout

--- frontend/src/pages/admin/restock/index.tsx
+++ frontend/src/pages/admin/restock/index.tsx
@@ -6,10 +6,13 @@ import { useNavigate } from "react-router-dom";
 import { ShowAllModal } from "./_components/all-items-modal";
 import { NoRestockState } from "./_components/no-restock-state";
 import {
+  Archive,
   Box,
+  BoxIcon,
   Calendar,
   CornerRightUp,
   Ellipsis,
+  Package,
   Pin,
   Truck,
 } from "lucide-react";
@@ -182,167 +185,170 @@ const RestockPage = () => {
             <NoRestockState />
           </div>
         ) : (
-          restockItems?.slice().sort((a, b) => new Date(b.created_At).getTime() - new Date(a.created_At).getTime()).map((r) => (
-            <div
-              key={r.restock_Id}
-              className="relative flex flex-col justify-between gap-5 border rounded-lg py-3 px-5 bg-custom-gray h-fit w-full break-inside-avoid"
-            >
-              <div className="absolute -top-1">
-                {r.status === "VOIDED" && (
-                  <div className="bg-red-500 text-white text-xs px-2 py-1 rounded-b-lg shadow-md">
-                    Voided
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-1 p-3">
-                <div className="flex flex-col gap-3 w-full">
-                  <div className="flex flex-col gap-2">
-                    <div className="flex items-center gap-5">
-                      <span className="text-lg font-semibold text-saltbox-gray tracking-wide">
-                        #{r.restock_Number}
-                      </span>
-                      <Pin className="text-amber-300 rotate-45" size={20} />
+          restockItems
+            ?.slice()
+            .sort(
+              (a, b) =>
+                new Date(b.created_At).getTime() -
+                new Date(a.created_At).getTime(),
+            )
+            .map((r) => (
+              <div
+                key={r.restock_Id}
+                className="relative flex flex-col justify-between gap-5 border rounded-lg py-3 px-5 bg-custom-gray h-fit w-full break-inside-avoid"
+              >
+                <div className="absolute -top-1">
+                  {r.status === "VOIDED" && (
+                    <div className="bg-red-500 text-white text-xs px-2 py-1 rounded-b-lg shadow-md">
+                      Voided
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-1 p-3">
+                  <div className="flex flex-col gap-3 w-full">
+                    <div className="flex flex-col gap-2">
+                      <div className="flex items-center gap-5">
+                        <span className="text-lg font-bold text-saltbox-gray tracking-wide">
+                          #{r.restock_Number}
+                        </span>
+                        <Pin className="text-amber-300 rotate-45" size={20} />
 
-                      <Popover>
-                        <PopoverTrigger asChild className="cursor-pointer ">
-                          <Ellipsis className="text-saltbox-gray" />
-                        </PopoverTrigger>
-                        <PopoverContent className="w-fit">
-                          <ul className="flex flex-col gap-1">
-                            <li className="text-sm cursor-pointer hover:underline">
-                              Print
-                            </li>
-                            <li className="text-sm cursor-pointer hover:underline">
-                              Export
-                            </li>
-                          </ul>
-                          <Separator orientation="horizontal" />
-                          <ul className="flex flex-col gap-1">
-                            <li className="text-sm cursor-pointer hover:underline ">
-                              View Restock
-                            </li>
-                            <li
-                              className="text-sm cursor-pointer hover:underline"
-                              onClick={() => handleViewSupplier(r)}
-                            >
-                              View Supplier
-                            </li>
-                          </ul>
-                          {r.status !== "VOIDED" && (
-                            <>
-                              <Separator orientation="horizontal" />
-                              <ul>
-                                <li
-                                  className={`text-sm hover:underline ${
-                                    isVoidingRestock
-                                      ? "text-red-300 cursor-not-allowed"
-                                      : "text-red-400 cursor-pointer"
-                                  }`}
-                                  // onClick={() => handleVoidRestock(r.restock_Id)}
-                                  onClick={() => handleVoidPrompt(r)}
-                                >
-                                  {isVoidingRestock ? "Voiding..." : "Void"}
-                                </li>
-                              </ul>
-                            </>
+                        <Popover>
+                          <PopoverTrigger asChild className="cursor-pointer ">
+                            <Ellipsis className="text-saltbox-gray" />
+                          </PopoverTrigger>
+                          <PopoverContent className="w-fit">
+                            <ul className="flex flex-col gap-1">
+                              <li className="text-sm cursor-pointer hover:underline">
+                                Print
+                              </li>
+                              <li className="text-sm cursor-pointer hover:underline">
+                                Export
+                              </li>
+                            </ul>
+                            <Separator orientation="horizontal" />
+                            <ul className="flex flex-col gap-1">
+                              <li className="text-sm cursor-pointer hover:underline ">
+                                View Restock
+                              </li>
+                              <li
+                                className="text-sm cursor-pointer hover:underline"
+                                onClick={() => handleViewSupplier(r)}
+                              >
+                                View Supplier
+                              </li>
+                            </ul>
+                            {r.status !== "VOIDED" && (
+                              <>
+                                <Separator orientation="horizontal" />
+                                <ul>
+                                  <li
+                                    className={`text-sm hover:underline ${
+                                      isVoidingRestock
+                                        ? "text-red-300 cursor-not-allowed"
+                                        : "text-red-400 cursor-pointer"
+                                    }`}
+                                    // onClick={() => handleVoidRestock(r.restock_Id)}
+                                    onClick={() => handleVoidPrompt(r)}
+                                  >
+                                    {isVoidingRestock ? "Voiding..." : "Void"}
+                                  </li>
+                                </ul>
+                              </>
+                            )}
+                          </PopoverContent>
+                        </Popover>
+                      </div>
+
+                      <div className="flex flex-col flex-1">
+                        <div className="flex gap-3 items-center">
+                          <Calendar className="text-saltbox-gray" size={18} />
+                          <span className="text-saltbox-gray text-sm">
+                            {new Intl.DateTimeFormat("en-US", {
+                              year: "numeric",
+                              month: "long",
+                              day: "numeric",
+                            }).format(new Date(r.created_At))}
+                          </span>
+                        </div>
+
+                        <div className="h-4 border-l-2 ml-2 border-l-saltbox-gray/70 my-1" />
+
+                        <div className="flex gap-3 items-center border-gray-300 ">
+                          <Truck className="text-saltbox-gray" scale={18} />
+                          <span className="text-saltbox-gray text-sm">
+                            {r.supplier.companyName}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center gap-2">
+                      <div className="p-2 rounded-md bg-white shrink-0 h-fit">
+                        <BoxIcon className="text-saltbox-gray" />
+                      </div>
+                      <div className="flex flex-col">
+                        <label className="text-nowrap text-sm font-semibold text-vesper-gray tracking-wide">
+                          Total Products
+                        </label>
+
+                        <span className="text-saltbox-gray text-base font-semibold">
+                          {r.line_Items.length}{" "}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                      <div className="p-2 rounded-md bg-white shrink-0 h-fit">
+                        <Archive className="text-saltbox-gray" />
+                      </div>
+                      <div className="flex flex-col">
+                        <label className="text-nowrap text-sm font-semibold text-vesper-gray tracking-wide">
+                          Total Quantity
+                        </label>
+
+                        <span className="text-saltbox-gray text-base font-semibold">
+                          {r.line_Items.reduce(
+                            (total, item) => total + item.base_Unit_Quantity,
+                            0,
                           )}
-                        </PopoverContent>
-                      </Popover>
-                    </div>
-
-                    <div className="flex items-center flex-1">
-                      <div className="flex gap-3 items-center">
-                        <Calendar className="text-saltbox-gray" size={18} />
-                        <span className="text-saltbox-gray text-sm">
-                          {new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                          }).format(new Date(r.created_At))}
-                        </span>
-                      </div>
-
-                      {/* <Separator orientation="vertical" /> */}
-
-                      <div className="flex gap-3 items-center ml-5 border-l-2 border-gray-300 pl-5">
-                        <Truck className="text-saltbox-gray" scale={18} />
-                        <span className="text-saltbox-gray text-sm">
-                          {r.supplier.companyName}
                         </span>
                       </div>
                     </div>
                   </div>
                 </div>
 
-                {/* <Separator orientation="vertical" /> */}
+                {/* <div className="bg-background rounded-lg flex flex-col p-3 gap-2">
+                  <div className="flex w-full justify-between ">
+                    <span className="text-saltbox-gray text-sm font-semibold">
+                      {r?.line_Items[0].product.product_Name}
+                    </span>
+                    <span className="text-saltbox-gray text-xl font-semibold">
+                      {r?.line_Items[0].base_Unit_Quantity}{" "}
+                    </span>
+                  </div>
 
-                <div className="flex flex-col justify-between">
-                  <span className="text-saltbox-gray text-xl font-semibold text-right">
-                    {r.line_Items.reduce(
-                      (total, item) => total + item.base_Unit_Quantity,
-                      0,
-                    )}
-                  </span>
-
-                  <span className="text-nowrap text-saltbox-gray text-sm font-semibold tracking-wide">
-                    Added Stock
-                  </span>
-                </div>
-
-                {/* <div className="bg-gray-bg border flex items-center justify-center rounded-lg p-2 h-12 w-12 my-auto">
-                  <EllipsisIcon />
+                  <div className="flex items-center gap-2 text-sm text-saltbox-gray">
+                    <Box className="" size={18} />
+                    <span className="text-sm text-saltbox-gray font-semibold">
+                      1 Conversions
+                    </span>
+                  </div>
                 </div> */}
-              </div>
 
-              <div className="bg-background rounded-lg flex flex-col p-3 gap-2">
-                <div className="flex w-full justify-between ">
-                  <span className="text-saltbox-gray text-sm font-semibold">
-                    {r?.line_Items[0].product.product_Name}
-                  </span>
-                  <span className="text-saltbox-gray text-xl font-semibold">
-                    {r?.line_Items[0].base_Unit_Quantity}{" "}
-                  </span>
-                </div>
-
-                <div className="flex items-center gap-2 text-sm text-saltbox-gray">
-                  <Box className="" size={18} />
-                  <span className="text-sm text-saltbox-gray font-semibold">
-                    1 Conversions
-                  </span>
-                </div>
-              </div>
-
-              <div className="bg-background rounded-lg flex items-center justify-between px-3 py-1">
-                <label className="text-saltbox-gray text-sm font-semibold tracking-wide">
-                  No more items...
-                </label>
-
+                {/* <div className="bg-background rounded-lg flex items-center justify-center px-3 py-1"> */}
                 <button
                   onClick={() => handleOpenModal(r)}
-                  className="bg-background text-saltbox-gray w-fit cursor-pointer hover:underline hover:shadow-none hover:bg-gray-300 transition-colors"
+                  className="bg-background text-saltbox-gray w-full max-w-full cursor-pointer hover:underline hover:shadow-none hover:bg-gray-300 transition-colors"
                 >
                   view details
                   <CornerRightUp size={18} />
                 </button>
-
-                {/* <div className="flex w-full justify-between ">
-                  <span className="text-saltbox-gray text-sm font-semibold">
-                    Eraser - Gamma Goods - black
-                  </span>
-                  <span className="text-saltbox-gray text-sm font-semibold">
-                    200
-                  </span>
-                </div>
-
-                <div className="flex items-center gap-2 text-sm text-saltbox-gray">
-                  <Box className="" size={18} />
-                  <span className="text-sm text-saltbox-gray font-semibold">
-                    1 Conversions
-                  </span>
-                </div> */}
+                {/* </div> */}
               </div>
-            </div>
-          ))
+            ))
         )}
       </div>
     </section>
